54803d66339b4c251e4d147b5eb4a89b3f5dacaf
Andrew Cheung <cheungdzinyung@gmail.com>
2020-03-21 23:21:50 +0800

    first commit

---
+++ src/humans.js
@@ -0,0 +1,22 @@
+const cards = [
+	"w1.jpg",
+	"w2.jpg",
+	"w3.jpg",
+	"w4.jpg",
+	"m1.jpg",
+	"m2.jpg",
+	"m3.jpg",
+	"m4.jpg",
+	"m5.jpg",
+	"w1ans.jpg",
+	"w2ans.jpg",
+	"w3ans.jpg",
+	"w4ans.jpg",
+	"m1ans.jpg",
+	"m2ans.jpg",
+	"m3ans.jpg",
+	"m4ans.jpg",
+	"m5ans.jpg"
+];
+
+export default cards;

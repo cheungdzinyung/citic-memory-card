b7bfd33d236a6088e618092a6b89d851c16d7788
Shing Lun Wong <shinglun.wong@gmail.com>
2020-05-11 00:04:58 +0800

    update game cards

---
+++ src/final.js
@@ -0,0 +1,12 @@
+const cards = [
+	"h1.jpg",
+	"h2.jpg",
+	"h3.jpg",
+	"h4.jpg",
+	"h1ans.jpg",
+	"h2ans.jpg",
+	"h3ans.jpg",
+	"h4ans.jpg",
+];
+
+export default cards;
--- src/humans.js
+++ src/humans.js
@@ -1,22 +1,12 @@
 const cards = [
-	"w1.jpg",
-	"w2.jpg",
-	"w3.jpg",
-	"w4.jpg",
-	"m1.jpg",
-	"m2.jpg",
-	"m3.jpg",
-	"m4.jpg",
-	"m5.jpg",
-	"w1ans.jpg",
-	"w2ans.jpg",
-	"w3ans.jpg",
-	"w4ans.jpg",
-	"m1ans.jpg",
-	"m2ans.jpg",
-	"m3ans.jpg",
-	"m4ans.jpg",
-	"m5ans.jpg"
+	"h1.png",
+	"h2.png",
+	"h3.png",
+	"h4.png",
+	"h1ans.png",
+	"h2ans.png",
+	"h3ans.png",
+	"h4ans.png",
 ];
 
 export default cards;

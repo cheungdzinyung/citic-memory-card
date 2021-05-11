9004a5c89aa8714d28b30612e2805f31df07fd51
Andrew Cheung <cheungdzinyung@gmail.com>
2021-05-11 17:46:00 +0800

    block out 1 card pair

--- src/humans.js
+++ src/humans.js
@@ -2,11 +2,11 @@ const cards = [
 	"h1.png",
 	"h2.png",
 	"h3.png",
-	"h4.png",
+	// "h4.png",
 	"h1ans.png",
 	"h2ans.png",
 	"h3ans.png",
-	"h4ans.png",
+	// "h4ans.png",
 ];
 
 export default cards;
